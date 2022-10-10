0d1c89ab2148d8cfbf1ef8b148b1d5752a5076bb
dev-mahfuz <mrsohag04@gmail.com>
2022-10-11 00:55:58 +0600

    Statistics page done

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
 const Statistics = () => {
     const topics = useLoaderData();
     return (
         <div className='container'>
             <h1 className='text-center my-3 mb-5'>Statistics</h1>
-            
+            <BarChart width={400} height={300} data={topics.data} className='m-auto'>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="total" fill="#8884d8" />
+            </BarChart>
         </div>
     );
 };
