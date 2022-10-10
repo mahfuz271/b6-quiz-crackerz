5d95ebf09d3de46dcb3807eb1bfdb824655d19d5
dev-mahfuz <mrsohag04@gmail.com>
2022-10-11 00:23:36 +0600

    quiz page done

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
-const Quiz = ({ quiz }) => {
+library.add(faEye);
+
+const Quiz = ({ quiz, showCorrentAns, checkCorrectAns }) => {
     return (
         <div className='col align-self-stretch'>
             <div className="border rounded h-100">
-                <h3 className="h4 mb-5 mt-3 text-center" dangerouslySetInnerHTML={{ __html: quiz.question }}></h3><FontAwesomeIcon icon="fa-solid fa-eye" />
-                <div className='row row-cols-1 row-cols-lg-2 g-3 px-5 mb-3'>
+                <h3 className="h4 mb-3 mt-3 text-center position-relative"><p dangerouslySetInnerHTML={{ __html: quiz.question }}></p><FontAwesomeIcon onClick={() => showCorrentAns(quiz.correctAnswer)} className='position-absolute faIconEye' icon={faEye} /></h3>
+                <p id={quiz.id} className="m-0 p-0 text-center alert alert-info d-none"></p>
+                <div className='row row-cols-1 row-cols-lg-2 g-3 px-5 mb-3 mt-3'>
                     {quiz.options.map((op, index) => {
-                        return <div className="form-check border py-3 ps-0 rounded">
-                            <input className="form-check-input mx-3" type="radio" name={`ans${quiz.id}`} id={`${quiz.id}${index}`} />
-                            <label className="form-check-label" for={`${quiz.id}${index}`}>
+                        return <div key={quiz.id + index} className="form-check border py-3 ps-0 rounded">
+                            <input onChange={() => checkCorrectAns(quiz.id, op, quiz.correctAnswer)} className="form-check-input mx-3" type="radio" name={`ans${quiz.id}`} id={`${quiz.id}${index}`} />
+                            <label className="form-check-label" htmlFor={`${quiz.id}${index}`}>
                                 {op}
                             </label>
                         </div>
--- src/Components/Quizes/Quizes.js
+++ src/Components/Quizes/Quizes.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Quizes = () => {
     const quizes = useLoaderData();
+    const [TotalCorrect, setTotalCorrect] = useState({});
+    const showCorrentAns = (ans) => {
+        toast(ans);
+    }
+
+    const checkCorrectAns = (id, selected, correct) => {
+        let txt, els = { ...TotalCorrect };
+        if (selected == correct) {
+            txt = 'Correct answer.';
+            els[id] = true;
+            setTotalCorrect(els);
+        } else {
+            txt = 'Wrong answer.';
+            delete els[id];
+            setTotalCorrect(els);
+        }
+        document.getElementById(id).innerHTML = txt;
+        document.getElementById(id).classList.remove('d-none');
+    }
+
     return (
         <div className='container'>
             <h1 className='text-center my-3'>Quiz of {quizes.data.name}</h1>
+            <p className='alert alert-success text-center'>Correct answers: {Object.keys(TotalCorrect).length}</p>
             <div className='row mt-3 row-cols-1 g-3 mb-5'>
-                {quizes.data.questions.map((q) => <Quiz key={q.id} quiz={q}></Quiz>)}
+                {quizes.data.questions.map((q) => <Quiz key={q.id} checkCorrectAns={checkCorrectAns} showCorrentAns={showCorrentAns} quiz={q}></Quiz>)}
             </div>
+            <ToastContainer />
         </div>
     );
 };
